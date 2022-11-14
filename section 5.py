321797dea43364e42bb4346ae9353f82a3ea8258
Sebastian Falah <sebastian.falah@toptal.com>
2022-11-14 15:43:16 -0500

    section 5 learning path

---
+++ section 5.py
@@ -0,0 +1,27 @@
+hello = 'Hello World!'
+
+print(hello)
+
+#### Hello You Section ####
+
+# Ask user for name
+
+name = input("What is your name?: ")
+
+# Ask user for age
+
+age = int(input("How old are you?: "))
+
+# Ask user of city
+
+city= input("What city do you live in?: ")
+
+# Ask user what they enjoy
+
+love= input("What do you love?:")
+
+# Create output text
+
+print("My name is",name, "I'm", age, "old" )
+
+# Print output to screen
---
+++ section3 and 4.py
@@ -0,0 +1,12 @@
+import random
+
+#Health potion project
+health = 50
+
+difficulty = 1
+
+potion_health = int(random.randint(25,50) / difficulty)
+
+health = health + potion_health
+
+print(health)
