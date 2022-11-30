a140cc3b95c9dc89f2d9e6b3ac62980030cda7f4
Sebastian Falah <sebastian.falah@toptal.com>
2022-11-30 09:41:38 -0500

    section 5 continued

--- section 5.py
+++ section 5.py
@@ -21,7 +21,32 @@ city= input("What city do you live in?: ")
 love= input("What do you love?:")
 
 # Create output text
-
-print("My name is",name, "I'm", age, "old" )
+string = "Your name is {} and you are {} old, you really love {} and you live in {}"
+output = string.format(name, age, love, city)
 
 # Print output to screen
+print(output)
+
+
+# First, create a variable called start, and set it equal to "I am ".
+# Remember the space after the word "am" !
+start = "I am "
+
+# Next, create a variable called age and set it equal to your age in years.
+# This must be a number
+age = 36
+
+# Next, create a variable called end, and set it equal to " years old".
+# Remember the space before the word "years"
+
+end = ' years old'
+
+# Next, create a variable called output and use {} symbols and the format() function to stick the start, age and end variables
+# together to make a string.
+output = '{}{}{}'.format(start, age, end)
+
+# An example output string would be "I am 15 years old"
+
+
+# Finally, print the output to the screen using the print() function.
+print(output)
